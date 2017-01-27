ddbb4a94fc8498331058bc65e171e0a1e23950b1
Bradley Falzon <brad@teambrad.net>
2017-01-27 12:59:24 +1030

    Add violations for unused, staticcheck and gosimple

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/pkg/errors"
 )
@@ -12,4 +13,9 @@ func main() {
 	if err != nil {
 		fmt.Println(errors.Wrap(err, "could not print")) // add external dependency
 	}
+}
+
+func fn1() { // warning: func fn1 is unused (U1000) (unused)
+	time.Parse("12345", "")    // warning: parsing time "12345" as "12345": cannot parse "" as "4" (SA1002) (staticcheck)
+	time.Now().Sub(time.Now()) // warning: should use time.Since instead of time.Now().Sub (S1012) (gosimple)
 }
