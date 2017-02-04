af8b271322ae0589fc9a0f3926d920b726f82ee6
Bradley Falzon <brad@teambrad.net>
2017-02-04 13:52:58 +1030

    Add

--- internal/pkg/pkg.go
+++ internal/pkg/pkg.go
@@ -2,4 +2,4 @@ package pkg
 
 // InternalPkgName is an internal packages constant that should be importable
 // following the Go internal package rules.
-const InternalPkgName = "fred "
+const InternalPkgName = "fred  "
